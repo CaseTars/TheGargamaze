c1bbb8ec96fbcf712cb465d2cfd6beeed619bbbe
Kevin Caio <k247218@dac.unicamp.br>
2022-06-04 17:51:09 -0300

    App-e-Control

--- core/src/com/mygdx/game/AppTheGargamaze.java
+++ core/src/com/mygdx/game/AppTheGargamaze.java
@@ -1,31 +1,43 @@
 package com.mygdx.game;
 
+import javax.xml.catalog.CatalogFeatures.Builder;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class AppTheGargamaze extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private View view;
+	private Itime Tcase;
+	private Itime Ttars;
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		
+		 Builder bob = new Builder();
+		 try {
+			 bob.build();
+		 }
+		 catch(Exception BuildError){
+			 //mensagem de erro e acabar o jogp
+			 System.exit(1);
+		 }
+		 
+		 view = bob.getView();
+		 Tcase = bob.getCase();
+		 Ttars = bob.getTars();
 	}
 
 	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void render () { //chamar o view
+		view.show();
+		Tcase.timeUpdate(Gdx.graphics.getDeltaTime());
+		Ttars.timeUpdate(Gdx.graphics.getDeltaTime());
 	}
 	
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose () { //view.dispose
+		view.dispose();
 	}
 }
---
+++ core/src/com/mygdx/game/Control.java
@@ -0,0 +1,68 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.math.Rectangle;
+
+public class Control implements InputProcessor {
+    private Icommand pCase;
+    private Icommand pTars;
+    
+    public void conectCase(Icommand pCase) {
+    	this.pCase = pCase;
+    }
+    
+    public void conectTars(Icommand pTars) {
+    	this.pTars = pTars;
+    }
+    
+    public boolean keyDown(int keycode) {
+        if(keycode == Input.Keys.LEFT)
+            pCase.moveLeft();
+        else if(keycode == Input.Keys.RIGHT)
+        	pCase.moveRight();
+        else if(keycode == Input.Keys.UP)
+        	pCase.moveUp();
+        else if(keycode == Input.Keys.DOWN)
+        	pCase.moveDown();
+        
+        else if(keycode == Input.Keys.A)
+        	pTars.moveLeft();
+        else if(keycode == Input.Keys.D)
+        	pTars.moveRight();
+        else if(keycode == Input.Keys.W)
+        	pTars.moveUp();
+        else if(keycode == Input.Keys.S)
+        	pTars.moveDown();
+        
+        return false;
+    }
+
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    public boolean touchDown(int x, int y, int pointer, int button) {
+        return false;
+    }
+
+    public boolean touchUp(int x, int y, int pointer, int button) {
+        return false;
+    }
+
+    public boolean touchDragged(int x, int y, int pointer) {
+        return false;
+    }
+
+    public boolean mouseMoved(int x, int y) {
+        return false;
+    }
+
+    public boolean scrolled(float amountX, float amountY) {
+        return false;
+    }
+}
