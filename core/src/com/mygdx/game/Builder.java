27ab2e2633464af5952f6a6966294aa06a58ced4
Thiago Maximo Pavão <thipavao44@gmail.com>
2022-06-04 18:13:04 -0300

    builder Inicial

--- core/src/com/mygdx/game/Builder.java
+++ core/src/com/mygdx/game/Builder.java
@@ -1,6 +1,34 @@
 package com.mygdx.game;
 
 public class Builder {
-
-	
+    private View view;
+    private Player pCase;
+    private Player pTars;
+    private Space space;
+    
+    public void build() {
+        view = new View();
+        space = new Space();
+        space.connectView(view);
+        
+        // leitura de arquivo
+        
+        pCase = new Player(0,0);
+        space.insert(pCase);
+        
+        pTars = new Player(1,0);
+        space.insert(pTars);
+    }
+    
+    View getView() {
+        return view;
+    }
+    
+    Player getCase() {
+        return pCase;
+    }
+    
+    Player getTars() {
+        return pTars;
+    }
 }
