34aae394312316fea9bced9b127888b0908dd028
Thiago Maximo Pavão <thipavao44@gmail.com>
2022-06-25 18:40:49 -0300

    backtomenuInGame

--- core/src/com/mygdx/game/elements/Player.java
+++ core/src/com/mygdx/game/elements/Player.java
@@ -18,7 +18,7 @@ public class Player extends Element implements IPlayer{
 	private ILantern lantern;
 	private boolean dead = false;
 	
-	private double timeRemaining = 300;
+	private double timeRemaining = 30;
 	private double totalTime = timeRemaining;
 	
 	private Array<IVisualEffect> effects = new Array<IVisualEffect>();
